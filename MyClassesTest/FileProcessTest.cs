d7e03997293a9d1cf4f5750d89ce5822c27410b9
Claes Engelin <claes.engelin@zocom.se>
2024-10-30 16:13:51 +0100

    Efter Modul 2
    Lagt till kött på benen på de första enhetstesterna

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -6,26 +6,68 @@ public class FileProcessTest
     [TestMethod]
     public void FileNameDoesExist()
     {
-        Assert.Inconclusive();
+        // Arrange
+        FileProcess fp = new();
+        string fileName = @"C:\Windows\Regedit.exe";
+        bool fromCall;
+
+        // Act
+        fromCall = fp.FileExists(fileName);
+
+        // Assert Assert.IsTrue(fromCall);
     }
+
 
     [TestMethod]
     public void FileNameDoesNotExist()
     {
-        Assert.Inconclusive();
+        // Arrange
+        FileProcess fp = new();
+        string fileName = @"C:\DoesNotExist.txt";
+        bool fromCall;
+
+        // Act
+        fromCall = fp.FileExists(fileName);
+
+        // Assert Assert.IsFalse(fromCall);
     }
 
-    [TestMethod]
-    public void
-    FileNameNullOrEmpty_UsingTryCatch_ShouldThrowArgumentNullException()
-    {
-        Assert.Inconclusive();
-    }
 
     [TestMethod]
-    public void
-    FileNameNullOrEmpty_UsingExpectedExceptionAttribute()
+    public void FileNameNullOrEmpty_UsingTryCatch_ShouldThrowArgumentNullException()
     {
-        Assert.Inconclusive();
+        // Arrange
+        FileProcess fp;
+        string fileName = string.Empty; bool fromCall = false;
+
+        try
+        {
+            // Act
+            fp = new();
+
+            fromCall = fp.FileExists(fileName);
+
+            // Assert: Fail as we should not get here Assert.Fail("The Call to the FileExists() method did NOT throw an ArgumentNullException and it SHOULD have.");
+        }
+        catch (ArgumentNullException)
+        {
+            // Assert: Test was a success Assert.IsFalse(fromCall);
+        }
     }
+
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void FileNameNullOrEmpty_UsingExpectedExceptionAttribute()
+    {
+        // Arrange
+        FileProcess fp = new();
+        string fileName = string.Empty; bool fromCall;
+
+        // Act
+        fromCall = fp.FileExists(fileName);
+
+        // Assert: Fail as we should not get here Assert.Fail("The Call to the FileExists() method did NOT throw an ArgumentNullException and it SHOULD have.");
+}
+
 }
